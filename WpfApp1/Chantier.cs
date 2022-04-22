5693b4ee0f93ffa96c931111a2236eab8d28ba5a
Rhaall <40182314+Rhaall@users.noreply.github.com>
2022-04-22 21:26:13 +0200

    v

---
+++ WpfApp1/Chantier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Chantier
+    {
+        int Id;
+        string Adresse;
+        string NomChantier;
+        string commentaire;
+        Devis[] devis;
+        Facture[] factures;
+
+
+    }
+}
---
+++ WpfApp1/Compagnons.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Compagnons
+    {
+        int ID;
+        string name;
+        string telephone;
+        int coutHoraire;
+        DateTime dateEmbauche;
+        string commentaire;
+    }
+}
