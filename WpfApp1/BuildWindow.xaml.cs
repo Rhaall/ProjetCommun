750c05fe1a09144b4d964adefb9cd71ed95ffdd3
arnaud.flores <arnaud.flores@epitech.eu>
2022-04-30 15:48:17 +0200

    add photo and restrict input in number field

--- WpfApp1/BuildWindow.xaml.cs
+++ WpfApp1/BuildWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace WpfApp1
 {
@@ -22,6 +23,31 @@ namespace WpfApp1
         public BuildWindow()
         {
             InitializeComponent();
+        }
+        private static readonly Regex _regex = new Regex( "[0-9]" );
+        private static bool IsTextAllowed(string text)
+        {
+            bool result = _regex.IsMatch( text );
+            return result;
+        }
+        private void HourRateTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextBox rateInput = sender as TextBox;
+            if(!IsTextAllowed( e.Text ) || rateInput.Text.Length > 3)
+            {
+                // event no longuer go further
+                e.Handled = true;
+            }
+                
+        }
+        private void TelNumberTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextBox rateInput = sender as TextBox;
+            if(!IsTextAllowed( e.Text ) || rateInput.Text.Length > 9)
+            {
+                // event no longuer go further
+                e.Handled = true;
+            }
         }
     }
 }
